c03b6cd863998bf766ad29eaf3f233b4267a0b88
Akashsharma98 <akashkrishan873@gmail.com>
2025-04-17 15:52:24 +0530

    Initial commit

--- utils/errorController.js
+++ utils/errorController.js
@@ -1,10 +1,42 @@
-module.exports = (err,req,res,next)=>{
-
+const sendErrorDev = (err,res)=>{
     const statusCode = err.statusCode || 500;
     res.status(statusCode).json({
         success:0,
-        message:err.message,
-        stack: err.stack
+        stack: err.stack,
+        message:err.message
     })
+}
+
+const sendErrorprod= (err,res)=>{
+    const statusCode = err.statusCode || 500;
+    if(isOprational){
+        res.status(statusCode).json({
+            success:0,
+            message:err.message,
+           
+        })
+    } else{
+        res.status(statusCode).json({
+            success:0,
+            message:"somthing  wrong",
+        
+        })
+    }      
+}
+
+
+
+module.exports = (err,req,res,next)=>{
+    if(process.env.NODE_env==='dev'){
+ sendErrorDev (err,res);
+     }else{
+        sendErrorprod(err,res)
+    }
+    // const statusCode = err.statusCode || 500;
+    // res.status(statusCode).json({
+    //     success:0,
+    //     message:err.message,
+    //     stack: err.stack
+    // })
 
 }
